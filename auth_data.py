7498383324a71ab16aa4ff188c302eefdb4a6c1f
Robert Williams <robccwilliams@gmail.com>
2017-05-07 01:25:51 +0100

    A much-needed refactor
    Functionality pulled out into a more rational filestructure rather than having everything in one meandering script

---
+++ auth_data.py
@@ -0,0 +1,20 @@
+from json import load as json_load
+from os import path
+
+
+class SpotifyAuthData:
+    """Holds the information necessary to create an authenticated Spotify service object"""
+
+    def __init__(self, filepath):
+        if not path.isfile(filepath):
+            print("Spotify auth data file does not exist.")
+            raise FileNotFoundError
+
+        with open(filepath, 'r') as file_in:
+            data = json_load(file_in)
+
+        self.username = data['username']
+        self.scope = data['scope']
+        self.client_secret = data['client_secret']
+        self.client_id = data['client_id']
+        self.redirect_uri = data['redirect_uri']
